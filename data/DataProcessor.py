ecfc5c90b8377c7205d17fc67495ad46abaa583e
Angelica Perez <aperez@thoughtworks.com>
2017-11-19 21:19:27 -0500

    Create data processor

---
+++ data/DataProcessor.py
@@ -0,0 +1,42 @@
+
+class DataProcessor:
+
+    def __init__(self):
+        self.labels = list()
+        self.sentences = list()
+        self.features = list()
+        self.readSentences('data/negativeSentences.txt', 'negative')
+        self.readSentences('data/positiveSentences.txt', 'positive')
+        self.getFeatures()
+
+    def readSentences(self, dataFile, label):
+        for line in open(dataFile):
+            words = line.strip().split(' ')
+            self.sentences.append(words)
+            self.labels.append(label)
+
+    def getFeatures(self):
+        for words in self.sentences:
+            featureVector = list()
+            featureVector.append(self.containsPositiveWord(words))
+            featureVector.append(self.containsNegativeWord(words))
+            self.features.append(featureVector)
+
+    def containsPositiveWord(self, words):
+        positiveWords = ['awesome', 'positive', 'party']
+        return self.containsWord(words, positiveWords)
+
+    def containsNegativeWord(self, words):
+        negativeWords = ['sad', 'no', 'negative']
+        return self.containsWord(words, negativeWords)
+
+    def containsWord(self, words, targetWords):
+        for word in words:
+            if word in targetWords:
+                return 1.0
+        return 0.0
+
+    def getData(self):
+        return self.features, self.labels
+
+
---
+++ main.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('data')
+from DataProcessor import DataProcessor
+
+dataProcessor = DataProcessor()
+data = dataProcessor.getData()
+
